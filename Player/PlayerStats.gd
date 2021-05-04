aac45fccfa4f6315447d872e27b6fd28f69db101
Alena Gainullina <82896548+tanukiCoder@users.noreply.github.com>
2021-05-04 17:37:18 +0500

    Add functional attack_kana change and pausing

--- Player/PlayerStats.gd
+++ Player/PlayerStats.gd
@@ -3,7 +3,7 @@ extends "res://Stats.gd"
 const DAMAGE_MODIFIER_WRONG_KANA: float = 0.5
 
 export(int) var player_damage = 2 
-var attack_kana: String = "tsu"
+var attack_kana: String = "tsu" setget set_player_attack_kana, get_player_attack_kana
 
 func _ready():
 	max_health = 10
@@ -14,3 +14,10 @@ func get_damage_for_kana(kana: String) -> int:
 		return player_damage
 	else:
 		return player_damage * DAMAGE_MODIFIER_WRONG_KANA
+
+func set_player_attack_kana(value: String):
+	attack_kana = value
+	print(attack_kana)
+
+func get_player_attack_kana():
+	return attack_kana
--- UI/KanaChangeMenu.gd
+++ UI/KanaChangeMenu.gd
@@ -1,5 +1,6 @@
 extends Sprite
 
+var is_in_pause_mode: bool = false
 var kanas = {
 	Vector2(1,1): "n",
 	Vector2(2,1): "wa",
@@ -7,15 +8,35 @@ var kanas = {
 	Vector2(11,1): "a",
 	Vector2(8,3): "tsu"
 }
+# append remaining letters
 
 onready var tiles = $TileMap
 
 func _ready():
-	pass # Replace with function body.
+	is_in_pause_mode = false
+	self.hide()
 
 func _input(event):
-	if event.is_pressed() && event is InputEventMouseButton:
+	if is_in_pause_mode && event is InputEventMouseButton && event.is_pressed():
 		var local_mouse_pos: Vector2 = get_local_mouse_position()
 		var tile_position: Vector2 = tiles.world_to_map(local_mouse_pos)
 		if kanas.has(tile_position):
-			print(kanas[tile_position])
+			PlayerStats.attack_kana = kanas[tile_position]
+			exit_pause_mode()
+	elif event.is_action_pressed("pause"):
+		if is_in_pause_mode:
+			exit_pause_mode()
+		else:
+			enter_pause_mode()
+
+func enter_pause_mode():
+	get_tree().paused = true
+	is_in_pause_mode = true
+	print("entered pause")
+	self.show()
+
+func exit_pause_mode():
+	self.hide()
+	is_in_pause_mode = false
+	get_tree().paused = false
+	print("pause exited")
