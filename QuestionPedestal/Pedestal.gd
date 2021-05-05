92d2fb01b5bc4348ff5bef0ceb05a7408ef9469a
Alena Gainullina <82896548+tanukiCoder@users.noreply.github.com>
2021-05-05 14:40:59 +0500

    Add question after interaction with pedestal

--- QuestionPedestal/Pedestal.gd
+++ QuestionPedestal/Pedestal.gd
@@ -1,11 +1,27 @@
 extends StaticBody2D
 
+enum ANSWER_STATE{
+	UNANSWERED = 0,
+	RIGHT_ANSWER = 1,
+	WRONG_ANSWER = 2
+}
+
 onready var sprite = $Sprite
+onready var question: PackedScene = load("res://QuestionPedestal/ControlQuestion.tscn")
+
+var is_answered: bool = false
+var q
+
+func _ready():
+	q = question.instance()
+	add_child(q)
+	q.randomize_and_fill_answers(3, ["e", "tsu", "fu"])
 
 func interact_with_player():
-	if (sprite.frame == 2):
-		sprite.frame = 0
-	else:
-		sprite.frame += 1
-	print("Interaction occured")
+	if !is_answered:
+		q.show_question()
+		var is_right_answer: bool = yield(q, "answer_made")
+		is_answered = true
+		sprite.frame = ANSWER_STATE.RIGHT_ANSWER if is_right_answer else ANSWER_STATE.WRONG_ANSWER
+		print("Interaction occured")
 
--- QuestionPedestal/Question.gd
+++ QuestionPedestal/Question.gd
@@ -1,8 +1,10 @@
-extends ColorRect
+extends MarginContainer
 
 const KANA_COUNT = 45
 
-onready var kanas = $Sprite
+signal answer_made(is_right)
+
+onready var kanas = $ColorRect/Sprite
 var answer_buttons = []
 
 var right_answer: String = ""
@@ -11,8 +13,13 @@ func _ready():
 	for button in get_tree().get_nodes_in_group("AnswerButtons"):
 		answer_buttons.append(button)
 		button.connect("pressed", self, "on_answer_pressed", [button])
+	hide()
 	# for test purposes
-	randomize_and_fill_answers(10, ["sa", "to", "wa"])
+	#randomize_and_fill_answers(10, ["sa", "to", "wa"])
+
+func show_question():
+	get_tree().paused = true
+	show()
 
 func randomize_and_fill_answers(kana_index: int, answers_array) -> void:
 	kanas.frame = kana_index
@@ -26,7 +33,10 @@ func randomize_and_fill_answers(kana_index: int, answers_array) -> void:
 func on_answer_pressed(button):
 	if button.text == right_answer:
 		print("Your answer was right")
+		emit_signal("answer_made", true)
 	else:
 		var template: String = "Wrong answer, \n this kana reads as '{right_answer}'"
 		print(template.format({"right_answer": right_answer}))
+		emit_signal("answer_made", false)
+	get_tree().paused = false
 	queue_free()
