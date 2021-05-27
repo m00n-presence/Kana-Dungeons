bf96d43a89461ec8bc9ddfebd971482785625a63
imdelette <gogifad@gmail.com>
2021-05-28 04:18:41 +0500

    added Kana Indicator

--- GUI/GUI.gd
+++ GUI/GUI.gd
@@ -1,14 +1,15 @@
 extends MarginContainer
 
-onready var number_label = $Bars/LifeBar/Count/Background/Number
+onready var kana_label = $Bars/KanaIndicator/KanaBackground/Hieroglyph
+onready var number_label = $Bars/LifeBar/Count/HPBackground/Number
 onready var bar = $Bars/LifeBar/TextureProgress
 onready var tween = $Tween
 
 var animated_health = 0
 
 func _ready():
-	$"/root/PlayerStats".connect("health_changed", self, "_health_changed")
-	$"/root/PlayerStats".connect("no_health_left", self, "_no_health_left")
+	$"/root/PlayerStats".connect("health_changed", self, "_on_PlayerStats_health_changed")
+	$"/root/PlayerStats".connect("no_health_left", self, "_on_PlayerStats_no_health_left")
 	var player_max_health = $"/root/PlayerStats".max_health
 	bar.max_value = player_max_health
 	update_health(player_max_health)
@@ -18,15 +19,18 @@ func _process(delta):
 	number_label.text = str(round_value)
 	bar.value = round_value
 	
-func update_health(health):
-	tween.interpolate_property(self, "animated_health", animated_health, health, 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN)
+func update_health(new_value):
+	tween.interpolate_property(self, "animated_health", animated_health, new_value, 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN)
 	if not tween.is_active():
 		tween.start()
 
-func _health_changed(current_health):
+func update_kana(new_kana):
+	kana_label.text = new_kana
+
+func _on_PlayerStats_health_changed(current_health):
 	update_health(current_health)
 
-func _no_health_left():
+func _on_PlayerStats_no_health_left():
 	var start_color = Color(1.0, 1.0, 1.0, 1.0)
 	var end_color = Color(1.0, 1.0, 1.0, 0.0)
 	tween.interpolate_property(self, "modulate", start_color, end_color, 1.0, Tween.TRANS_LINEAR, Tween.EASE_IN)
