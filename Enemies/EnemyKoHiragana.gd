4389a34392fff9262d49fc54f5c8c01eb6bbf754
tanuki <tauonmuon76@gmail.com>
2021-07-14 14:00:11 +0500

    Add new signals to detection zone and new behavior to ko_enemy

--- Enemies/EnemyKoHiragana.gd
+++ Enemies/EnemyKoHiragana.gd
@@ -22,11 +22,17 @@ onready var sprite = $Sprite
 onready var wanderController = $WanderController
 onready var stats = $Stats
 onready var hurtbox = $Hurtbox
+onready var hitBox = $Hitbox
+
 var kana_spirit: PackedScene
 
 func _ready():
 	self.set_physics_process(false)
 	kana_spirit = null # will make spritesheet later
+	detectionZone.connect("player_entered_zone", self, "on_DetectionZone_entered")
+	attackZone.connect("player_entered_zone", self, "on_AttackZone_entered")
+	detectionZone.connect("player_left_zone", self, "on_DetectionZone_left")
+	attackZone.connect("player_left_zone", self, "on_AttackZone_left")
 
 func _physics_process(delta):
 	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
@@ -34,11 +40,11 @@ func _physics_process(delta):
 	match state:
 		STATES.IDLE:
 			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
-			seek_player()
+			#seek_player()
 			if wanderController.get_time_left() == 0:
 				randomize_state_and_start_wander_time()
 		STATES.WANDER:
-			seek_player()
+			#seek_player()
 			if wanderController.get_time_left() == 0:
 				randomize_state_and_start_wander_time()
 			change_velocity(delta, wanderController.target_position)
@@ -50,14 +56,19 @@ func _physics_process(delta):
 				change_velocity(delta, player.global_position)
 			else:
 				state = STATES.IDLE
-	sprite.flip_h = velocity.x < 0
+		STATES.ATTACK:
+			var player = attackZone.player
+			if player != null:
+				make_attack(player)
+			else:
+				on_AttackZone_left()
 	velocity = move_and_slide(velocity)
 
 func seek_player() -> void:
-	if detectionZone.can_see_player():
-		state = STATES.CHASE
-	elif attackZone.can_see_player():
+	if attackZone.can_see_player():
 		state = STATES.ATTACK
+	elif detectionZone.can_see_player():
+		state = STATES.CHASE
 
 func randomize_state_and_start_wander_time() -> void:
 	state = STATES.values()[randi() % 2] 
@@ -66,3 +77,21 @@ func randomize_state_and_start_wander_time() -> void:
 func change_velocity(delta, to_position: Vector2) -> void:
 	var dir: Vector2 = global_position.direction_to(to_position)
 	velocity = velocity.move_toward(dir * SPEED, ACCELERATION * delta)
+
+func make_attack(player_body):
+	var direction_to_player: Vector2 = global_position.direction_to(player_body.global_position)
+
+func on_DetectionZone_entered(player_body):
+	state = STATES.CHASE
+
+func on_AttackZone_entered(player_body):
+	state = STATES.ATTACK
+
+func on_AttackZone_left():
+	if detectionZone.can_see_player():
+		state = STATES.CHASE
+	else:
+		state = STATES.IDLE
+
+func on_DetectionZone_left():
+	state = STATES.IDLE
--- Enemies/PlayerDetectionZone.gd
+++ Enemies/PlayerDetectionZone.gd
@@ -1,12 +1,17 @@
 extends Area2D
 
+signal player_entered_zone(player_body)
+signal player_left_zone
+
 var player = null
 
 func _on_PlayerDetectionZone_body_entered(body):
 	player = body
+	emit_signal("player_entered_zone", player)
 
 func _on_PlayerDetectionZone_body_exited(_body):
 	player = null
+	emit_signal("player_left_zone")
 
 func can_see_player() -> bool:
 	return player != null
